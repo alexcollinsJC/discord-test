518678d7f700ba8ce31fb9186a2b7f32f48ac03d
Alex Collins <alex.collins@jamcity.com>
2022-01-21 22:10:18 +0000

    Use Environment Variable For Discord Bot Token

--- DiscordTest/DiscordBot.cs
+++ DiscordTest/DiscordBot.cs
@@ -16,8 +16,7 @@ namespace DiscordTest
 
             //  You can assign your bot token to a string, and pass that in to connect.
             //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
-            var token = "OTM0MjA0NjcwMzYzMTkzNDA0.Yesr5g.k0uLW18vWq5OpUwnQL0rQmPlb88";
-
+            var token = System.Environment.GetEnvironmentVariable("DiscordBotToken");
             // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
             // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
             // var token = File.ReadAllText("token.txt");
