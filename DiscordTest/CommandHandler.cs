28d451172ce7e3920d1e5eea06df96662b1318a5
Alex Collins <alex.collins@jamcity.com>
2022-01-24 13:19:14 -0800

    Fix Indentation In Files
     - Use top-level namespace syntax

--- DiscordTest/CommandHandler.cs
+++ DiscordTest/CommandHandler.cs
@@ -4,66 +4,65 @@ using System.Text;
 using Discord;
 using Discord.WebSocket;
 
-namespace DiscordTest
+namespace DiscordTest;
+
+public class CommandHandler
 {
-    public class CommandHandler
+    private readonly DiscordSocketClient client;
+    private readonly CommandService commandService;
+
+    public CommandHandler(DiscordSocketClient discordClient, CommandService commands)
     {
-        private readonly DiscordSocketClient client;
-        private readonly CommandService commandService;
+        client = discordClient;
+        commandService = commands;
+    }
 
-        public CommandHandler(DiscordSocketClient discordClient, CommandService commands)
+    public async Task InstallCommandAsync()
+    {
+        client.MessageReceived += HandleCommandAsync;
+        commandService.CommandExecuted += OnCommandExecuted;
+
+        await commandService.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+
+        StringBuilder sb = new();
+        sb.AppendLine("Installed Commands:");
+        foreach (ModuleInfo moduleInfo in commandService.Modules)
         {
-            client = discordClient;
-            commandService = commands;
-        }
-
-        public async Task InstallCommandAsync()
-        {
-            client.MessageReceived += HandleCommandAsync;
-            commandService.CommandExecuted += OnCommandExecuted;
-
-            await commandService.AddModulesAsync(Assembly.GetEntryAssembly(), null);
-
-            StringBuilder sb = new();
-            sb.AppendLine("Installed Commands:");
-            foreach (ModuleInfo moduleInfo in commandService.Modules)
+            // [module1]
+            //   command1(param1, param2)
+            //   command2(param3, param4)
+            sb.AppendLine($"[{moduleInfo.Name}]");
+            foreach (CommandInfo ci in moduleInfo.Commands)
             {
-                // [module1]
-                //   command1(param1, param2)
-                //   command2(param3, param4)
-                sb.AppendLine($"[{moduleInfo.Name}]");
-                foreach (CommandInfo ci in moduleInfo.Commands)
-                {
-                    sb.AppendLine($"  {ci.Name}({string.Join(", ", ci.Parameters.Select(pi => $"{pi.Type.Name} {pi.Name}"))})");
-                }
+                sb.AppendLine($"  {ci.Name}({string.Join(", ", ci.Parameters.Select(pi => $"{pi.Type.Name} {pi.Name}"))})");
             }
-
-            Console.Write(sb.ToString());
         }
 
-        private static Task OnCommandExecuted(Optional<CommandInfo> command, ICommandContext context, IResult result)
+        Console.Write(sb.ToString());
+    }
+
+    private static Task OnCommandExecuted(Optional<CommandInfo> command, ICommandContext context, IResult result)
+    {
+        string commandName = command.IsSpecified ? $"'{command.Value.Name}'" : "A command";
+        Console.WriteLine(new LogMessage(LogSeverity.Info,
+            "CommandExecution",
+            $"{commandName} was executed at {DateTime.UtcNow} with result: {(result.IsSuccess ? "Success" : result.Error?.ToString())}."));
+
+        return Task.CompletedTask;
+    }
+
+    private async Task HandleCommandAsync(SocketMessage message)
+    {
+        if (message is not SocketUserMessage userMessage) return;
+
+        int argPos = 0;
+        if (userMessage.Author.IsBot || !(userMessage.HasCharPrefix('!', ref argPos) ||
+                                          userMessage.HasMentionPrefix(client.CurrentUser, ref argPos)))
         {
-            string commandName = command.IsSpecified ? $"'{command.Value.Name}'" : "A command";
-            Console.WriteLine(new LogMessage(LogSeverity.Info,
-                "CommandExecution",
-                $"{commandName} was executed at {DateTime.UtcNow} with result: {(result.IsSuccess ? "Success" : result.Error?.ToString())}."));
-
-            return Task.CompletedTask;
+            return;
         }
 
-        private async Task HandleCommandAsync(SocketMessage message)
-        {
-            if (message is not SocketUserMessage userMessage) return;
-
-            int argPos = 0;
-            if (userMessage.Author.IsBot || !(userMessage.HasCharPrefix('!', ref argPos) ||
-                                              userMessage.HasMentionPrefix(client.CurrentUser, ref argPos)))
-            {
-                return;
-            }
-
-            SocketCommandContext context = new(client, userMessage);
-            await commandService.ExecuteAsync(context, argPos, null);
-        }
+        SocketCommandContext context = new(client, userMessage);
+        await commandService.ExecuteAsync(context, argPos, null);
     }
 }
--- DiscordTest/DiscordBot.cs
+++ DiscordTest/DiscordBot.cs
@@ -3,72 +3,71 @@ using Discord.WebSocket;
 using System;
 using Discord.Commands;
 
-namespace DiscordTest
+namespace DiscordTest;
+
+public class DiscordBot
 {
-    public class DiscordBot
+    private DiscordSocketClient client;
+
+    public DiscordBot()
     {
-        private DiscordSocketClient client;
+        client = new DiscordSocketClient();
 
-        public DiscordBot()
+        client.Log += Log;
+        client.Ready += OnReady;
+    }
+
+    public async Task Connect()
+    {
+        string? token = Environment.GetEnvironmentVariable("DiscordBotToken");
+
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+
+    private static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+
+    // Put test code here
+    private async Task OnReady()
+    {
+        await LoadCommands();
+        Test();
+    }
+
+    private async Task LoadCommands()
+    {
+        CommandService commandService = new(new CommandServiceConfig
         {
-            client = new DiscordSocketClient();
-            
-            client.Log += Log;
-            client.Ready += OnReady;
-        }
+            LogLevel = LogSeverity.Verbose
+        });
 
-        public async Task Connect()
+        CommandHandler commandHandler = new(client, commandService);
+        await commandHandler.InstallCommandAsync();
+    }
+
+    private async void Test()
+    {
+        await Task.Delay(1000);
+        await SendMessage();
+    }
+
+    public async Task SendMessage()
+    {
+        SocketChannel channel = client.GetChannel(934211004487852042);
+
+        if (channel is IMessageChannel messageChannel)
         {
-            string? token = Environment.GetEnvironmentVariable("DiscordBotToken");
+            Console.WriteLine("Sending message...");
+            IUserMessage sentMessage = await messageChannel.SendMessageAsync("Hello World!");
 
-            await client.LoginAsync(TokenType.Bot, token);
-            await client.StartAsync();
-
-            // Block this task until the program is closed.
-            await Task.Delay(-1);
-        }
-
-        private static Task Log(LogMessage msg)
-        {
-            Console.WriteLine(msg.ToString());
-            return Task.CompletedTask;
-        }
-
-        // Put test code here
-        private async Task OnReady()
-        {
-            await LoadCommands();
-            Test();
-        }
-
-        private async Task LoadCommands()
-        {
-            CommandService commandService = new(new CommandServiceConfig
-            {
-                LogLevel = LogSeverity.Verbose
-            });
-
-            CommandHandler commandHandler = new(client, commandService);
-            await commandHandler.InstallCommandAsync();
-        }
-
-        private async void Test()
-        {
-            await Task.Delay(1000);
-            await SendMessage();
-        }
-
-        public async Task SendMessage()
-        {
-            SocketChannel channel = client.GetChannel(934211004487852042);
-
-            if (channel is IMessageChannel messageChannel)
-            {
-                Console.WriteLine("Sending message...");
-                IUserMessage sentMessage = await messageChannel.SendMessageAsync("Hello World!");
-
-                Console.WriteLine($"Message sent: {sentMessage}");
-            }
+            Console.WriteLine($"Message sent: {sentMessage}");
         }
     }
 }
